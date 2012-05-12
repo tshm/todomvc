3f68d0efb18e479cf9e31521684d4fb4afbed6cc
tosh shimayama <2sm@csc.jp>
2012-05-11 20:25:27 -0400

    clean up / syntax fixes

--- architecture-examples/angularjs/js/app.js
+++ architecture-examples/angularjs/js/app.js
@@ -1,50 +1,60 @@
 (function( window ) {
 	'use strict';
 
-	var angular = window.angular,
-	  undefined;
+	var angular = window.angular;
 
 	/** angular controller */
-	var TodoController = function($scope, $filter, $location) {
+	var TodoController = function( $scope, $filter, $location ) {
 		// array of todo items
 		$scope.todos = [];
 
-		// active/completed todo filtering
+		// routing handler: active/completed todo filtering
 		$scope.$location = $location;
-		$scope.$watch('$location.path()', function(newval) {
+		$scope.$watch( '$location.path()', function( newval ) {
 			var filter;
-			if (newval.length > 3) {
-				filter = {completed: ( newval === '/completed' ? true : false)};
+			if ( newval.length > 3 ) {
+				filter = {
+					completed: ( newval === '/completed' ? true : false )
+				};
 			}
 			$scope.displayFilter = filter;
 		});
 
 		$scope.addTodo = function() {
-			$scope.todos.push({name: $scope.newItem, completed: false});
+			$scope.todos.push({
+				name: $scope.newItem,
+				completed: false
+			});
 			$scope.newItem = '';
 		};
 
-		$scope.destroyTodo = function(index) {
-			$scope.todos.splice(index, 1);
+		$scope.destroyTodo = function( index ) {
+			$scope.todos.splice( index, 1 );
 		};
 
 		$scope.remainingCount = function() {
-			return $filter('filter')($scope.todos, {completed: true}).length;
+			return $filter('filter')( $scope.todos, {
+				completed: true
+			}).length;
 		};
 
 		$scope.clearCompleted = function() {
-			$scope.todos = $filter('filter')($scope.todos, {completed: false});
+			$scope.todos = $filter('filter')( $scope.todos, {
+				completed: false
+			});
 		};
 		
 		$scope.allCompleted = function() {
 			var all = $scope.todos.length,
-			    completed = $filter('filter')($scope.todos, {completed: true}).length;
+				completed = $filter('filter')( $scope.todos, {
+					completed: true
+				}).length;
 			return (all > 0) && (all === completed);
 		};
 
 		$scope.toggleCompletion = function() {
 			var newval = !$scope.allCompleted();
-			angular.forEach($scope.todos, function(todo) {
+			angular.forEach( $scope.todos, function(todo) {
 				todo.completed = newval;
 			});
 		};
