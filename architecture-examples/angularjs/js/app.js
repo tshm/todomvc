9d5665c8834ecb515a73f6b5e45a4efb7dd54ada
tosh shimayama <2sm@csc.jp>
2012-05-14 13:14:57 -0400

    using directive but not working...

--- architecture-examples/angularjs/js/app.js
+++ architecture-examples/angularjs/js/app.js
@@ -3,6 +3,44 @@
 
 	var angular = window.angular;
 
+	var module = angular.module('TodoApp', []);
+
+	/** custom directive for blur/keydown event handling */
+	module.directive('todoCommit', function() {
+		var ENTER_KEY = 13;
+		return function( scope, elem, attrs ) {
+			var runCommit = function(event) {
+				scope.$apply(attrs['todoCommit']);
+				event.stopPropagation();
+			};
+			elem.bind('blur', runCommit );
+			elem.bind('keydown', function( event ) {
+				if ( event.which === ENTER_KEY ) {
+					runCommit(event);
+				}
+			});
+			/*
+			scope.$on('activate', function() {
+				scope.todo.editing = true;
+				elem[0].focus();
+				elem[0].select();
+				console.log(['activate triggered', elem, scope]);
+			});
+			*/
+		};
+	});
+
+	/** custom directive to handle select on focus */
+	module.directive('todoFocus', function() {
+		return function( scope, elem, attrs ) {
+			scope.$watch(attrs['todoFocus'], function(newval) {
+				console.log(typeof newval);
+				console.log(['focus', elem, arguments]);
+				//if (newval) { elem[0].focus(); elem[0].select(); }
+			});
+		};
+	});
+
 	/** angular controller */
 	var TodoController = function( $scope, $filter, $location ) {
 		// array of todo items
@@ -58,6 +96,22 @@
 				todo.completed = newval;
 			});
 		};
+
+		$scope.editing = function(todo) {
+			todo.editing = true;
+			$scope.$broadcast('activate');
+		};
+
+		$scope.rename = function( index ) {
+			console.log(['rename called', arguments]);
+			var newname = $scope.todos[index].name;
+			if ( !newname ) {
+				$scope.destroyTodo( index );
+			} else {
+				delete $scope.todos[index].editing;
+			}
+		};
+
 	};
 	// export
 	window.TodoController = TodoController;
